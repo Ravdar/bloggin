ae04406e38181ac53f72d2fe74bf224eb39a8da5
Ravdar <97836782+Ravdar@users.noreply.github.com>
2024-02-22 20:47:45 +0100

    topics form added

--- mainapp/forms.py
+++ mainapp/forms.py
@@ -1,10 +1,11 @@
 from django import forms
-from .models import Article, Comment, Subcomment
+from .models import Article, Comment, Subcomment, Topic
 
 class NewArticle(forms.ModelForm):
+    topics = forms.ModelMultipleChoiceField(queryset=Topic.objects.all(), widget=forms.CheckboxSelectMultiple)
     class Meta:
         model = Article
-        fields = ["title", "body","image"]
+        fields = ["title", "body","image", "topics"]
 
 class NewComment(forms.ModelForm):
     class Meta:
