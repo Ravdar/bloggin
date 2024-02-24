e3a7b638d30464876e6934e071e462fe59348574
Ravdar <97836782+Ravdar@users.noreply.github.com>
2024-02-24 14:52:24 +0100

    trending and new articles section done, including resposiveness

--- mainapp/static/main.js
+++ mainapp/static/main.js
@@ -5,14 +5,12 @@ trendingArticlesButton.addEventListener("click", function () {
     if (trendingArticlesButton.textContent === "+") {
         console.log("show");
         trendingArticlesContainer.style.maxHeight = "2000px";
-        trendingArticlesContainer.style.padding = "10px";
-        trendingArticlesContainer.style.transition = "1s ease-in-out";
+        trendingArticlesContainer.style.transition = "maxHeight 1s ease-in-out";
         trendingArticlesButton.textContent = "-";
     }
     else {
         trendingArticlesContainer.style.maxHeight = "0";
-        trendingArticlesContainer.style.padding = "0px";
-        trendingArticlesContainer.style.transition = "0.5s ease-in-out";
+        trendingArticlesContainer.style.transition = " maxHeight0.5s ease-in-out";
         console.log("hide"); trendingArticlesButton.textContent = "+";
     }
 })
