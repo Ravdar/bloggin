0fc85b74f067b6966f87c6a1a78e0642e2f0aac8
Ravdar <97836782+Ravdar@users.noreply.github.com>
2024-02-26 20:02:34 +0100

    working on new articles frontend

--- mainapp/static/main.js
+++ mainapp/static/main.js
@@ -1,16 +1,35 @@
+// GENERAL FUNCTIONS
+
+// Showing and hidding window on button click
+function showHideContainer(showHideButton, affectedContainer) {
+    if (showHideButton.textContent === "+") {
+        affectedContainer.style.maxHeight = "2000px";
+        affectedContainer.style.transition = "maxHeight 1s ease-in-out";
+        showHideButton.textContent = "-";
+    }
+    else {
+        affectedContainer.style.maxHeight = "0";
+        affectedContainer.style.transition = " maxHeight 0.5s ease-in-out";
+        console.log("hide"); showHideButton.textContent = "+";
+    }
+}
+
+// Making trending section responsive
 var trendingArticlesButton = document.querySelector(".trending-button");
 var trendingArticlesContainer = document.querySelector(".trending-articles");
 
 trendingArticlesButton.addEventListener("click", function () {
-    if (trendingArticlesButton.textContent === "+") {
-        console.log("show");
-        trendingArticlesContainer.style.maxHeight = "2000px";
-        trendingArticlesContainer.style.transition = "maxHeight 1s ease-in-out";
-        trendingArticlesButton.textContent = "-";
-    }
-    else {
-        trendingArticlesContainer.style.maxHeight = "0";
-        trendingArticlesContainer.style.transition = " maxHeight 0.5s ease-in-out";
-        console.log("hide"); trendingArticlesButton.textContent = "+";
-    }
-})
+    showHideContainer(trendingArticlesButton, trendingArticlesContainer);
+});
+
+
+// NEW ARTICLE PAGE
+
+// Show and hide topics checkboxes
+var showTopicsButton = document.getElementById("show-topics");
+var topicsContainer = document.querySelector(".available-topics");
+
+showTopicsButton.addEventListener("click", function () { showHideContainer(showTopicsButton, topicsContainer) });
+
+
+
