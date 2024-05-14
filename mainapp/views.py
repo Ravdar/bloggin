04cba92ba2ca1bf6d450ff7f42abe63ce14e5dc8
ravdar <8squeex8@gmail.com>
2024-05-14 16:10:39 +0200

    Added comments in views.py

--- mainapp/views.py
+++ mainapp/views.py
@@ -9,46 +9,28 @@ from .models import Article, Comment, ArticleVote, Topic, Subcomment
 from .forms import NewArticle, NewComment, NewSubcomment
 
 def main_view(request):
+    """Home page view, returns all articles ordered from the newest, list of top articles and available topics.
+    """
     all_articles = Article.objects.all()[::-1]
     top_articles = all_articles[:10]
     topics = Topic.objects.all()
     return render(request, "mainapp/main.html", {"all_articles":all_articles, "top_articles":top_articles, "topics":topics})
 
-# Handling AJAX requests on articles votes
-def vote_article(request):
-    if request.method == 'POST' and request.is_ajax():
-        article_id = request.POST.get('article_id')
-        is_upvote = request.POST.get('is_upvote')
-        article = Article.objects.get(pk=article_id)
-        user = request.user
 
-        # Check if the user has already voted on this article
-        existing_vote = ArticleVote.objects.filter(user=user, article=article).first()
-        if existing_vote:
-            existing_vote.is_upvote = is_upvote
-            existing_vote.save()
-        else:
-            # Create a new vote
-            ArticleVote.objects.create(user=user, article=article, is_upvote=is_upvote)
-
-        # Update the article's vote count
-        article.update_total_votes()
-
-        # Return JSON response with updated vote counts
-        return JsonResponse({'success': True, 'upvotes': article.upvotes, 'downvotes': article.downvotes})
-    return JsonResponse({'success': False, 'error': 'Invalid request'})
-
-
-# login required only for adding comments
 def read_article_view(request, article_url):
+    """View for displaying article content and comment/subcomment forms."""
+
+    # Acces specific article and it's comments
     article = get_object_or_404(Article, url=article_url)
     comments = Comment.objects.filter(article=article)
 
+    # Handling submitting a comment
     if request.method == "POST":
         comment_form = NewComment(request.POST)
         subcomment_form = NewSubcomment(request.POST)
         form_type = request.POST.get('form_type')
         if form_type == 'comment_form':
+            # If submit is a comment
             if comment_form.is_valid():
                 new_comment = comment_form.save(commit=False)
                 new_comment.publication_date = timezone.now()
@@ -56,6 +38,7 @@ def read_article_view(request, article_url):
                 new_comment.owner = request.user
                 new_comment.save()            
         elif form_type == 'subcomment_form':
+            # If submit is subcomment
             if subcomment_form.is_valid():
                 parent_comment_id = request.POST.get('parent_comment_id')
                 parent_comment = get_object_or_404(Comment, pk=parent_comment_id)
@@ -74,6 +57,7 @@ def read_article_view(request, article_url):
             "subcomment_form": subcomment_form
         })
     else:
+        # Display article and comments forms (GET request)
         comment_form = NewComment()
         subcomment_form = NewSubcomment()
 
@@ -84,8 +68,12 @@ def read_article_view(request, article_url):
         "subcomment_form": subcomment_form
     })
 
+
 @login_required
 def new_article_view(request):
+    """View for adding new article."""
+
+    # Submitting article (POST request)
     if request.method == "POST":
         form = NewArticle(request.POST, request.FILES)
         if form.is_valid():
@@ -94,15 +82,22 @@ def new_article_view(request):
             new_article.owner = request.user
             new_article = form.save()
             return HttpResponseRedirect(reverse("mainapp:read_article_view", args=(new_article.url,)))
+    #GET request
     else:
         form=NewArticle()
         return render(request, "mainapp/new_article.html", {"form":form})
 
+
 @login_required
 def edit_article_view(request, article_url):
+    """View for editing already existing article."""
+
+    # Acces specific article
     article = get_object_or_404(Article, url=article_url)
+    # Check if user is the owner of the article, so he can edit it
     if article.owner != request.user:
         raise Http404
+    # POST request
     if request.method =="POST":
         form = NewArticle(request.POST, request.FILES, instance=article)
         if form.is_valid():
@@ -110,11 +105,16 @@ def edit_article_view(request, article_url):
             article.last_edit_date = timezone.now()
             article = form.save()
             return HttpResponseRedirect(reverse("mainapp:read_article_view", args=(article.url,)))
+    # Displaying article content in form (GET request)
     else:
         form = NewArticle(instance=article)
     return render(request, "mainapp/edit_article_view.html", {"form": form})
 
+
 def user_profile(request, user_username):
+    """Displaying profile of specific user, his info such as nickname, profile picture, added articles and comments."""
+
+    # Acces user and his activities
     user = get_object_or_404(User, username = user_username)
     activities = []
     articles = Article.objects.filter(owner=user)
