eed8ca8cb489a2126cbb58e96f30146d0cc886ec
ravdar <8squeex8@gmail.com>
2024-05-16 20:52:37 +0200

    Changes in comment section profile pictures display

---
+++ mainapp/utils.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from users.models import Profile
+
+def make_existing_users_profiles():
+
+    existing_users = User.objects.all()
+
+
+    for user in existing_users:
+
+        if not Profile.objects.filter(user=user).exists():
+            Profile.objects.create(user=user)
